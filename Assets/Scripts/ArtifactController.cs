7f173470d6b4df79b6785ab249f547359d8d096c
GaryTheKid <55671706+GaryTheKid@users.noreply.github.com>
2021-11-26 04:41:37 -0600

    Update to HDRP

--- Assets/Scripts/ArtifactController.cs
+++ Assets/Scripts/ArtifactController.cs
@@ -7,15 +7,44 @@ public class ArtifactController : MonoBehaviour
     public Transform player;
 
     [SerializeField] private GameObject menu;
+    private Color menuColor;
 
-    public void ShowMenu() 
+    public void ShowMenu()
     {
-        menu.SetActive(true);
+        StartCoroutine(ShowMenu_Coroutine());
     }
 
     public void HideMenu() 
     {
+        StartCoroutine(HideMenu_Coroutine());
+    }
+
+    IEnumerator ShowMenu_Coroutine()
+    {
+        menu.SetActive(true);
+        while (menuColor.a < 1)
+        {
+            menuColor = new Color(menuColor.r, menuColor.b, menuColor.g, menuColor.a + Time.deltaTime);
+            menu.GetComponent<MeshRenderer>().material.color = menuColor;
+            yield return new WaitForEndOfFrame();
+        } 
+    }
+
+    IEnumerator HideMenu_Coroutine()
+    {
+        while (menuColor.a >= 0)
+        {
+            menuColor = new Color(menuColor.r, menuColor.b, menuColor.g, menuColor.a - Time.deltaTime);
+            menu.GetComponent<MeshRenderer>().material.color = menuColor;
+            yield return new WaitForEndOfFrame();
+        }
         menu.SetActive(false);
+    }
+
+    private void Start()
+    {
+        menuColor = menu.GetComponent<MeshRenderer>().material.color;
+        print(menuColor);
     }
 
     private void Update()
@@ -23,6 +52,7 @@ public class ArtifactController : MonoBehaviour
         if (menu.activeSelf)
         {
             menu.transform.LookAt(player);
+            menu.transform.localEulerAngles = new Vector3(0f, menu.transform.localEulerAngles.y, 0f);
         }
     }
 }
